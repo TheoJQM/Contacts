bd47fd8846d2dbc14bcea58ffae53c96fb75f58f
TheoJQM <tjacqu01@gmail.com>
2024-04-11 10:40:53 +0200

    import and export contact in json format finished

--- Contacts (Kotlin)/task/src/Main.kt
+++ Contacts (Kotlin)/task/src/Main.kt
@@ -1,13 +1,17 @@
 package contacts
 
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
 
 abstract class Record(var name: String) {
+    abstract val type: String
     lateinit var timeCreated: String
     lateinit var timeLastEdit: String
 
@@ -15,7 +19,7 @@ abstract class Record(var name: String) {
         get() = if (field == "") "[no number]" else field
         set(value) {
             val phoneRegex = Regex("""\+?(\(?\p{Alnum}+\)?|\p{Alnum}+[ -]\(?\p{Alnum}{2,}\)?)([ -]\p{Alnum}{2,})*""")
-            field = if (phoneRegex.matches(value.trim()))  value.trim() else { println("Wrong number format!"); "" }
+            field = if (phoneRegex.matches(value.trim()) || value == "[no number]")  value.trim() else { println("Wrong number format!"); "" }
         }
 
     abstract fun editRecord()
@@ -34,18 +38,19 @@ abstract class Record(var name: String) {
 }
 
 class Person(name: String, private var surname: String) : Record(name) {
+    override val type: String = "person"
     var birthDate: String = ""
         get() = if (field == "") "[no data]" else field
         set(value) {
             val dateRegex = Regex("""\d{4}-\d{2}-\d{2}""")
-            field = if (dateRegex.matches(value.trim())) value.trim() else { println("Bad birth date!"); "" }
+            field = if (dateRegex.matches(value.trim()) || value == "[no data]") value.trim() else { println("Bad birth date!"); "" }
         }
 
     var gender: String = ""
         get() = if (field == "") "[no data]" else field
         set(value) {
             val genderRegex = Regex("""[MF]""")
-            field = if (genderRegex.matches(value.trim())) value.trim() else { println("Bad gender!"); "" }
+            field = if (genderRegex.matches(value.trim()) || value == "[no data]") value.trim() else { println("Bad gender!"); "" }
         }
 
     override fun editRecord() {
@@ -83,6 +88,7 @@ class Person(name: String, private var surname: String) : Record(name) {
 }
 
 class Organisation(name: String, private var address: String) : Record(name) {
+    override val type: String = "person"
 
     override fun editRecord() {
         val field = print("Select a field (address, number): ").run { readln() }
@@ -111,9 +117,22 @@ class Organisation(name: String, private var address: String) : Record(name) {
     }
 }
 
-class Contact {
+class Contact(args: Array<String>) {
     private val records = mutableListOf<Record>()
     private var exit = true
+    private var importFile = File("")
+    private var exportFile = File("./contact.db")
+
+    init {
+        if (args.size == 2 && args[0] == "--file" && File(args[1]).exists()) {
+            importFile = File(args[1])
+            exportFile = File(args[1])
+
+            import()
+
+            println("open ${importFile.name}\n")
+        }
+    }
 
     fun useContact() {
         while (exit) {
@@ -122,8 +141,9 @@ class Contact {
                 "add" -> addRecord()
                 "list" -> showRecords()
                 "search" -> search()
+                "export" -> export()
                 "count" -> countRecords()
-                "exit" -> exit = false
+                "exit" -> exit()
             }
         }
     }
@@ -159,6 +179,7 @@ class Contact {
         person.timeLastEdit = formatter.format(LocalDateTime.now())
 
         records.add(person)
+        export()
         println("The record added.\n")
     }
 
@@ -175,6 +196,7 @@ class Contact {
         organisation.timeCreated = formatter.format(LocalDateTime.now())
         organisation.timeLastEdit = formatter.format(LocalDateTime.now())
         records.add(organisation)
+        export()
         println("The record added.\n")
     }
 
@@ -182,7 +204,7 @@ class Contact {
 
     private fun showRecords() {
         for (i in records.indices) println("${i + 1}. ${records[i].showName()}")
-        val index = print("[list] Enter action ([number], back): ").run { readln() }
+        val index = print("\n[list] Enter action ([number], back): ").run { readln() }
         if (index == "back") return else updateRecord(index.toInt() - 1)
     }
 
@@ -190,50 +212,103 @@ class Contact {
         var again = true
         while (again) {
             val query = print("Enter search query: ").run { readln().lowercase() }
-            val resultIndices = records.filter { it.getAllValue().contains(query.trim()) }.indices.toList()
+            val resultIndices = records.filter { it.getAllValue().contains(query.trim()) }.map { records.indexOf(it) }.toList()
 
             if (resultIndices.isEmpty()) {
                 println("No result\n")
             } else {
                 println("Found ${resultIndices.size} results:")
-                for (i in resultIndices.indices) println("${i + 1}. ${records[i].showName()}")
+                for (i in resultIndices.indices) println("${i + 1}. ${records[resultIndices[i]].showName()}")
             }
 
             val action = print("[search] Enter action ([number], back, again): ").run { readln() }
-            when (action) {
-                "back" -> again = false
+            again = when (action) {
+                "back" -> false
                 "again" -> continue
                 else -> updateRecord(action.toInt() - 1)
             }
         }
     }
 
-    private fun updateRecord(index: Int) {
-        var record = true
+    private fun updateRecord(index: Int): Boolean {
+        val record = true
 
         println(records[index].toString() + "\n")
 
         while (record) {
             val action = print("[record] Enter action (edit, delete, menu): ").run { readln() }
             when (action) {
-                "edit" -> records[index].editRecord()
-                "delete" -> deleteRecord(index)
-                "menu" -> record = false
+                "edit" -> {
+                    records[index].editRecord()
+                    export()
+                    return true
+                }
+                "delete" -> {
+                    deleteRecord(index)
+                    return true
+                }
+                "menu" ->  {
+                    println()
+                    return false
+                }
+
+                else -> return true
             }
         }
+        return false
     }
 
     private fun deleteRecord(index: Int) {
         records.removeAt(index)
+        export()
         println("Record removed!\n")
     }
 
 
 
     private fun countRecords() = println("The Phone Book has ${records.size} records.\n")
+
+    private fun exit() {
+        exit = false
+        if (exportFile.exists()) exportFile.delete()
+    }
+
+
+
+    private fun import() {
+        val content = importFile.readText()
+        val moshi = Moshi.Builder()
+            .add(
+                PolymorphicJsonAdapterFactory.of(Record::class.java, "type")
+                    .withSubtype(Person::class.java, "person")
+                    .withSubtype(Organisation::class.java, "organisation")
+            )
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        val recordsAdapter = moshi.adapter<List<Record>>(Types.newParameterizedType(List::class.java, Record::class.java)).indent("  ")
+
+        records.addAll(recordsAdapter.fromJson(content)!!)
+
+    }
+
+    private fun export() {
+        val moshi = Moshi.Builder()
+            .add(
+                PolymorphicJsonAdapterFactory.of(Record::class.java, "type")
+                    .withSubtype(Person::class.java, "person")
+                    .withSubtype(Organisation::class.java, "organisation")
+            )
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        val recordsAdapter = moshi.adapter<List<Record>>(Types.newParameterizedType(List::class.java, Record::class.java)).indent("  ")
+        exportFile.writeText(recordsAdapter.toJson(records.toList()))
+    }
 }
 
-fun main() {
-    val contacts = Contact()
+fun main(args: Array<String>) {
+    val contacts = Contact(args)
     contacts.useContact()
+
 }
